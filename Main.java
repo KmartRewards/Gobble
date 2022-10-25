d8872e898a231fc6ca6dd917f3aef1fd39efcdb5
KmartRewards <awu35158@gmail.com>
2022-10-25 13:38:25 -0500

    buton

--- Main.java
+++ Main.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Main {
+package Goggle;
+public class Main {
+    public static void main(String[]args) {
+        System.out.println("Hello");
+    }
 }
--- src/Goggle/Main.java
+++ src/Goggle/Main.java
@@ -1,5 +1,12 @@
+package Goggle;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
 public class Main {
+    private JButton exit;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.printf("Welcome to %s.", Variables.name);
+        Scanner s = new Scanner(System.in);
+        GUIOpen();
     }
 }
